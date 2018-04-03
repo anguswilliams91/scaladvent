b2516b3243188d2efe43d78533b51dbee352a7a8
anguswilliams91 <anguswilliams91@gmail.com>
2018-04-03 15:00:51 +0100

    puzzle 7

--- puzzle_7/puzzleSeven.scala
+++ puzzle_7/puzzleSeven.scala
@@ -2,12 +2,63 @@ import scala.io.Source
 
 object puzzleSeven {
 
+  case class DiscTree(name: String, discWeight: Int, children: List[DiscTree]) {
+
+    val isEmpty = false
+
+    override def toString: String =
+      "DiscTree(Name: " + name + ", Weight: " + discWeight + ", " + "Children: {" + children.map(_.toString).foldRight("")(_ + ", " + _) + " })"
+
+    lazy val childWeights: List[Int] = {
+
+      def treeListWeights(trees: List[DiscTree]): List[Int] = trees match {
+        case Nil => Nil
+        case x :: xs => (x.discWeight + treeListWeights(x.children).foldLeft(0)(_ + _)) :: treeListWeights(xs)
+      }
+
+      treeListWeights(children)
+    }
+
+    lazy val isBalanced: Boolean = childWeights.distinct.size == 1
+
+    lazy val adjustment: Int = this.isBalanced match {
+      case true => 0
+      case false => {
+        val valsAndCounts = childWeights.distinct.map(x => (x, childWeights.filter(y => y == x).size))
+        valsAndCounts.filter(_._2 == 1)(0)._1 - valsAndCounts.filter(_._2 != 1)(0)._1
+      }
+    }
+
+    lazy val brokenTree: Int =
+      childWeights.indexOf(
+        childWeights.distinct.map(x => (x, childWeights.filter(y => y == x).size)).filter(_._2 == 1)(0)._1
+      )
+
+    lazy val correctValue: Int = {
+
+      def correctValueRec(tree: DiscTree): Int = {
+        val imbalanced = tree.isBalanced
+        imbalanced match {
+          case true => tree.discWeight - adjustment
+          case false => correctValueRec(tree.children(tree.brokenTree))
+        }
+      }
+
+      if (this.isBalanced) throw new Exception("This tree is balanced!")
+      else correctValueRec(this.children(brokenTree))
+
+    }
+
+  }
+
   def main(args: Array[String]): Unit = {
+
+    //Parse the data from the given format
 
     val nameRegex = """([a-z]+)""".r
     val weightRegex = """(\d+)""".r
 
-    def makeSubtree(input: String): (String, Int, List[String]) = {
+    def parseRow(input: String): (String, Int, List[String]) = {
       val names = nameRegex.findAllIn(input)
       val weight = weightRegex.findAllIn(input).map(_.toInt)
       val thisName = names.next
@@ -18,8 +69,10 @@ object puzzleSeven {
     }
 
     def parseData(path: String): List[(String, Int, List[String])] = {
-      Source.fromFile(path).getLines.toList.map(makeSubtree)
+      Source.fromFile(path).getLines.toList.map(parseRow)
     }
+
+    // Part 1: find the root of the tree
 
     def findRoot(parsedData: List[(String, Int, List[String])]): String = {
       val parents = parsedData.map(_._1).toSet
@@ -27,10 +80,31 @@ object puzzleSeven {
       (parents diff children).head
     }
 
-    println(findRoot(parseData("puzzle_7_input.txt")))
+    val data = parseData("puzzle_7_input.txt")
+    val root = findRoot(data)
+    println(root)
 
+    // Part 2: find the correct weight for the broken disc
 
+    def makeTree(parsedData: List[(String, Int, List[String])]): DiscTree = {
+
+      val root = findRoot(parsedData)
+
+      def makeTreeRec(treeName: String): DiscTree = {
+        val (name, discWeight, children) = parsedData.filter(_._1 == treeName)(0)
+        children match {
+          case Nil => DiscTree(name, discWeight, Nil)
+          case _ => DiscTree(name, discWeight, children.map(makeTreeRec))
+        }
+      }
+
+      makeTreeRec(root)
+    }
+
+    val tree = makeTree(data)
+    println(tree.correctValue)
 
   }
+
 
 }
