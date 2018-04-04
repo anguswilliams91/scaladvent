5099871c70f55fd7dccc08a18220fece8c84fe57
anguswilliams91 <anguswilliams91@gmail.com>
2018-04-04 15:19:10 +0100

    ...

--- puzzle_9/puzzleNine.scala
+++ puzzle_9/puzzleNine.scala
@@ -1,26 +1,29 @@
 import scala.io.Source
+import scala.util.matching
 
 
 object puzzleNine{
 
   def main(args: Array[String]): Unit = {
 
-    val bangPattern = """!.{1}""".r
-    def removeBangs(input: String): String = bangPattern.replaceAllIn(input, "")
+    def removeBangs(input: String): String =
 
     def removeGarbage(input: String): String = {
-      val inputNoBangs = removeBangs(input)
+      val bangPattern = """!.{1}""".r
       val opener = """<""".r
       val closer = """>""".r
+      val inputNoBangs = bangPattern.replaceAllIn(input, "")
 
-      def removeGarbageAcc(input: String, pattern: Regex) = opener.findFirstMatchIn(input) match {
-        case Some(patMatch) => {
-
+      def removeGarbageRec(input: String): String = opener.findFirstMatchIn(input) match {
+        case Some(openMatch) => {
+          val closeMatch = closer.findFirstMatchIn(input).get
+          val toRemove = input slice (openMatch.start, closeMatch.start + 1)
+          removeGarbageRec(input.replace(toRemove, ""))
         }
         case None => input
       }
 
-      removeGarbageAcc(inputNoBangs, opener)
+      removeGarbageRec(inputNoBangs)
     }
 
 
