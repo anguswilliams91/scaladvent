d95e20252dd74eb490ff667ed3df8d7452ad1a20
anguswilliams91 <anguswilliams91@gmail.com>
2018-04-04 17:38:36 +0100

    finish 9

--- puzzle_9/puzzleNine.scala
+++ puzzle_9/puzzleNine.scala
@@ -2,29 +2,49 @@ import scala.io.Source
 import scala.util.matching
 
 
-object puzzleNine{
+object puzzleNine {
 
   def main(args: Array[String]): Unit = {
 
-    def removeBangs(input: String): String =
-
-    def removeGarbage(input: String): String = {
+    def removeGarbage(input: String): (String, Int) = {
       val bangPattern = """!.{1}""".r
       val opener = """<""".r
       val closer = """>""".r
       val inputNoBangs = bangPattern.replaceAllIn(input, "")
 
-      def removeGarbageRec(input: String): String = opener.findFirstMatchIn(input) match {
-        case Some(openMatch) => {
-          val closeMatch = closer.findFirstMatchIn(input).get
-          val toRemove = input slice (openMatch.start, closeMatch.start + 1)
-          removeGarbageRec(input.replace(toRemove, ""))
+      def removeGarbageRec(input: String, acc: Int): (String, Int) = input.indexOf('<') match {
+        case -1 => (input, acc)
+        case _ => {
+          val leftPos = input indexOf '<'
+          val rightPos = input indexOf '>'
+          val before = input slice(0, leftPos)
+          val after = input slice(rightPos + 1, input.size)
+          removeGarbageRec(before + after, acc + rightPos - leftPos - 1)
         }
-        case None => input
       }
 
-      removeGarbageRec(inputNoBangs)
+      removeGarbageRec(inputNoBangs, 0)
     }
+
+    def getScore(input: String): (Int, Int) = {
+      val (cleanedInput, numChars) = removeGarbage(input)
+
+      def getScoreRec(input: List[Char], numOpen: Int, score: Int): Int = input match {
+        case Nil => score
+        case x :: xs => {
+          x match {
+            case '{' => getScoreRec(xs, numOpen + 1, score)
+            case '}' => getScoreRec(xs, numOpen - 1, score + numOpen)
+            case _ => getScoreRec(xs, numOpen, score)
+          }
+        }
+      }
+
+      (getScoreRec(cleanedInput.toList, 0, 0), numChars)
+    }
+
+    val data = Source.fromFile("puzzle_9_input.txt").mkString
+    println(getScore(data))
 
 
   }
