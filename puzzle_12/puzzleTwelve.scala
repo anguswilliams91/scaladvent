998fa84a03c8ee30666204847924168b92c14223
anguswilliams91 <anguswilliams91@gmail.com>
2018-04-09 14:54:00 +0100

    12

---
+++ puzzle_12/puzzleTwelve.scala
@@ -0,0 +1,30 @@
+import scala.io.Source
+
+object puzzleTwelve {
+
+  def parseLine(line: String): Seq[Set[Int]] = {
+    val splitLine = line.split("<->")
+    val parentNode = splitLine(0).trim.toInt
+    val childNodes = splitLine(1).split(',').map(_.trim.toInt)
+    childNodes.map(Set(parentNode, _))
+  }
+
+  def findFriends(id: Int, pairs: Set[Set[Int]]): List[Int] = {
+    val seedSet = pairs.filter(_ contains id)
+    val seeds = seedSet.flatMap(_ - id)
+    seeds.toList.flatMap(findGroup(_, pairs -- seedSet)) ++ seeds.toList
+  }
+
+  def findGroup(id: Int, pairs: Set[Set[Int]]): Set[Int] = findFriends(id, pairs).toSet + id
+
+  def findAllGroups(pairs: Set[Set[Int]]): Set[Set[Int]] = pairs.flatMap(_.toList).map(findGroup(_, pairs)).toSet
+
+  def main(args: Array[String]): Unit = {
+
+    val data = Source.fromFile("puzzle_12_input.txt").getLines.flatMap(parseLine).toSet
+    println(findGroup(0, data).size)
+    println(findAllGroups(data).size)
+
+  }
+
+}
